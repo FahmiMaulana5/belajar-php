027c97d195e456a0ccbd6d3b8854ed40ac82c4f8
Fahmi Maulana Fadila <92506040+FahmiMaulana5@users.noreply.github.com>
2022-09-07 21:06:11 +0700

    Add files via upload

---
+++ rumus-persegi-panjang.php
@@ -0,0 +1,25 @@
+<form action="rumus-persegi-panjang.php" method="POST">
+    <h1>Rumus Luas dan Keliling Persegi Panjang</h1>
+    <p>Panjang :</p>
+    <input type="number" name="panjang" placeholder="Ex.5" />
+    <br>
+    <p>Lebar :</p>
+    <input type="number" name="lebar" placeholder="Ex.5" />
+    <input type="submit" name="proses" value="Hitung Luas & Keliling" />
+</form>
+
+<?php
+    if(isset ($_POST["proses"])){
+        echo "<hr>";
+        $panjang = $_POST["panjang"];
+        $lebar = $_POST["lebar"];
+        $luas = $panjang * $lebar;
+        $keliling = 2 * ($panjang + $lebar);
+
+        echo "Panjang : $panjang <br>";
+        echo "Lebar : $lebar <br>";
+        echo "Luas Persegi Panjang : $luas <br>";
+        echo "Keliling Persegi Panjang : $keliling <br>";
+    }
+
+?>
